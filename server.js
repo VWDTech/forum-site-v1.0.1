5b0e40345a22acbe52ac7cf5ef593fff79d6d6c9
VWDTech <vwdvirtualworld@gmail.com>
2024-07-31 17:07:27 +0300

    version 1.0.1

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = app.listen(3000);
+
+const io = socketio(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (client) =>  {
+    client.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    });
+});
